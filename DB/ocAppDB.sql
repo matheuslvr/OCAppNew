7e02b4de7c087b82cec4c06091b8e5ebbbb65c3d
Marco Silva <marcowilliamdf@gmail.com>
2015-06-12 18:53:40 -0700

    Creating task_status in the Database

--- DB/ocAppDB.sql
+++ DB/ocAppDB.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 12, 2015 at 05:09 AM
+-- Generation Time: Jun 13, 2015 at 03:52 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -51,16 +51,38 @@ CREATE TABLE IF NOT EXISTS `Task` (
   `task_type_id` int(11) NOT NULL,
   `task_description` text NOT NULL,
   `task_time` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL
+  `category_id` int(11) NOT NULL,
+  `task_date` date NOT NULL,
+  `task_status_id` int(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Task`
 --
 
-INSERT INTO `Task` (`task_id`, `task_name`, `task_type_id`, `task_description`, `task_time`, `category_id`) VALUES
-(1, 'Task1', 1, 'Task1Description', 20, 1),
-(2, 'Task2', 2, 'Task2Description', 40, 2);
+INSERT INTO `Task` (`task_id`, `task_name`, `task_type_id`, `task_description`, `task_time`, `category_id`, `task_date`, `task_status_id`) VALUES
+(1, 'Task1', 1, 'Task1Description', 20, 1, '2015-06-01', 1),
+(2, 'Task2', 2, 'Task2Description', 40, 2, '2015-06-03', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Task_status`
+--
+
+CREATE TABLE IF NOT EXISTS `Task_status` (
+  `task_status_id` int(11) NOT NULL,
+  `task_status_description` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Task_status`
+--
+
+INSERT INTO `Task_status` (`task_status_id`, `task_status_description`) VALUES
+(1, 'To do'),
+(2, 'In Progress'),
+(3, 'Done');
 
 -- --------------------------------------------------------
 
@@ -110,7 +132,8 @@ CREATE TABLE IF NOT EXISTS `User` (
   `user_id` int(11) NOT NULL,
   `user_login` text NOT NULL,
   `user_password` text NOT NULL,
-  `user_name` text NOT NULL,
+  `user_first_name` text NOT NULL,
+  `user_last_name` text NOT NULL,
   `user_type_id` int(11) NOT NULL,
   `user_tel` text,
   `user_mail` text NOT NULL
@@ -120,10 +143,10 @@ CREATE TABLE IF NOT EXISTS `User` (
 -- Dumping data for table `User`
 --
 
-INSERT INTO `User` (`user_id`, `user_login`, `user_password`, `user_name`, `user_type_id`, `user_tel`, `user_mail`) VALUES
-(1, 'admin', '*4ACFE3202A5FF5CF467898FC58AAB1D615029441', 'Administrator', 1, '0000000000', 'admin@admin.com'),
-(2, 'manager', '*7D2ABFF56C15D67445082FBB4ACD2DCD26C0ED57', 'Manager', 2, '1111111111', 'manager@manager.com'),
-(3, 'teammember', '*2EA75752B55BD59CF97094DCAA594A11BABB21D7', 'Team Member', 3, '22222222222', 'teammember@teammember.com');
+INSERT INTO `User` (`user_id`, `user_login`, `user_password`, `user_first_name`, `user_last_name`, `user_type_id`, `user_tel`, `user_mail`) VALUES
+(1, 'admin', '*4ACFE3202A5FF5CF467898FC58AAB1D615029441', 'Admin', 'Admin', 1, '0000000000', 'admin@admin.com'),
+(2, 'manager', '*7D2ABFF56C15D67445082FBB4ACD2DCD26C0ED57', 'Manager', 'Manager', 2, '1111111111', 'manager@manager.com'),
+(3, 'teammember', '*2EA75752B55BD59CF97094DCAA594A11BABB21D7', 'Teammember', 'Teammember', 3, '22222222222', 'teammember@teammember.com');
 
 -- --------------------------------------------------------
 
@@ -203,6 +226,12 @@ ALTER TABLE `Task`
   ADD PRIMARY KEY (`task_id`);
 
 --
+-- Indexes for table `Task_status`
+--
+ALTER TABLE `Task_status`
+  ADD PRIMARY KEY (`task_status_id`);
+
+--
 -- Indexes for table `Task_type`
 --
 ALTER TABLE `Task_type`
@@ -253,6 +282,11 @@ ALTER TABLE `Category`
 --
 ALTER TABLE `Task`
   MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `Task_status`
+--
+ALTER TABLE `Task_status`
+  MODIFY `task_status_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `Task_type`
 --
