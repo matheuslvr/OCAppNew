72b8efdb18aabbc234cf09a6e5d35ad65a835695
Marco Silva <marcowilliamdf@gmail.com>
2015-06-14 13:33:39 -0700

    BarTest.php done

--- charts4php-free-latest/demos/basic/barTest.php
+++ charts4php-free-latest/demos/basic/barTest.php
@@ -9,17 +9,68 @@
  
 include("../../lib/inc/chartphp_dist.php");
 
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "ocAppDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully <br>";
+
+
+// Execute query and get the variables from the database
+$counter = 0;
+
+$sql5 = "SELECT category_id, category_description FROM Category";
+$result5 = mysqli_query($conn, $sql5);
+if (mysqli_num_rows($result5) > 0) {
+	while ($row = mysqli_fetch_assoc($result5)) { //run while the lines are not empty
+		$task_time_temp = 0;
+		$category_id_temp = $row["category_id"];
+		$category_name_task[$counter] = $row["category_description"];
+		$sql6 = "SELECT task_time FROM Task WHERE category_id = '{$category_id_temp}'";
+		$result6 = mysqli_query($conn, $sql6);
+		if (mysqli_num_rows($result6) > 0) {
+			while ($row = mysqli_fetch_assoc($result6)) {
+				$task_time_temp = $task_time_temp + $row["task_time"];
+			}
+			$time_task[$counter] = $task_time_temp;
+		}
+		else{
+			$time_task[$counter] = 0;
+		}
+		$counter++;
+	}
+}
+
 $p = new chartphp();
 
-$p->data = array(array(array("2010/10",48.25),array("2011/01",238.75),array("2011/02",95.50),array("2011/03",300.50),array("2011/04",286.80),array("2011/05",400)));
+$arrlength = count($category_name_task);
+// creating the arrays
+for($x = 0; $x < $arrlength; $x++){
+
+$arrays[$x] = array($category_name_task[$x], (int)$time_task[$x]);
+
+}
+
+$p->data = array($arrays); // Set the arrays as the data to print the graph
+
 $p->chart_type = "bar";
 
 // Common Options
-$p->title = "Bar Chart";
-$p->xlabel = "My X Axis";
-$p->ylabel = "My Y Axis";
+$p->title = "Categories x Hours";
+$p->xlabel = "";
+$p->ylabel = "Hours";
 
 $out = $p->render('c1');
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -27,10 +78,17 @@ $out = $p->render('c1');
 		<script src="../../lib/js/jquery.min.js"></script>
 		<script src="../../lib/js/chartphp.js"></script>
 		<link rel="stylesheet" href="../../lib/js/chartphp.css">
+
+	<style>
+		/* white color data labels */
+		.jqplot-data-label{color:white;}
+	</style>
 	</head>
+	
 	<body>
 		<div style="width:40%; min-width:450px;">
-			<?php echo $out; ?>
+		<?php echo $out; ?>
+
 		</div>
 	</body>
 </html>
